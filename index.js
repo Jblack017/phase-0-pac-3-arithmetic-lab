7e73734736ffc28f65f4938cb458a4bc0530e9e0
Jblack017 <blackhamjoshua@comcast.net>
2021-03-04 14:37:36 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+    1 + 2
+}
+
+function subtract() {
+    1 - 2
+}
+
+function multiply() {
+    1 * 2
+}
+
+function divide() {
+    1 / 2
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    return ++a
+}
+
+function decrement(a) {
+    return --a
+}
+
+function makeInt(a) {
+    return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
+
